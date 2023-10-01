0f5f98af9e4f45b4cb96fb323cd852df5061f6da
will-lancer <133719988+will-lancer@users.noreply.github.com>
2023-10-01 19:12:26 -0400

    Update error calc div.py

--- Lab template/error calc div.py
+++ Lab template/error calc div.py
@@ -15,17 +15,15 @@ delta_first = float(input("error first: ")) #error first
 delta_second = float(input("error second: ")) #error second
 
 final_error = (first_parameter)/(second_parameter) * ( ((delta_first)/(first_parameter))**2 + 
-((delta_second)/(second_parameter))**2)**(1/2)
+((delta_second)/(second_parameter))**2 )**(1/2)
 
 #^^error calculation
 
-final_val = (first_parameter)/(second_parameter)
-
-#final value
+final_val = (first_parameter)/(second_parameter) #final value
 
 
-print("final value:" + str(final_val)) #prints final value
-print("error:" + str(final_error)) #prints final error
+print("final value: " + str(final_val)) #prints final value
+print("error: " + str(final_error)) #prints final error
 
 #choose first and second parameters carefully, as I'm assuming the
 #first divides the second.
